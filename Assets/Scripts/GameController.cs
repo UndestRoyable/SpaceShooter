e6bd844f58f631c266c7203157f464927165eae3
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-23 20:08:34 +0200

    Spawning asteroids on random locations.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;
+	public Vector3 spawnValues; // used to set random spawn position on X-axis.
+
+	void Start()
+	{
+		SpawnWaves (); //Calls SpawnWaves function on the start of the game
+	}
+	void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
+		//Generates random hazard. y and z are static values, x is Randomly generated
+		//by Ramdon.Range function
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+}
