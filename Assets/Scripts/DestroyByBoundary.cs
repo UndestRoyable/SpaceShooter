4b86621277efea831805cc259fddb3f244be7e02
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-23 18:35:33 +0200

    Destroying objects leaving game boundary.
    Every object which leave the boundary of the screen is being destroyed
    by OnTriggerExit's Destroy function

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour 
+{
+	void OnTriggerExit(Collider other)
+	{
+		Destroy (other.gameObject);
+	}
+	//When the object (bolt or other)leave the boundary of the screen, its being destroyed.
+}
