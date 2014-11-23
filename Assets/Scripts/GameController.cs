fb15302e2a572ff4a78c159d36e198b18a73f449
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-23 15:50:07 +0200

    SpaceShip added. MeshColider and Rigidbody added.
    All assets are imported

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float InitialHazardSpeed;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText GameOverText;
+	public GUIText GameStats;
+	public GUIText HighScoreText;
+
+	private int score;
+	private int shotsFired;
+	private int totalHazardsSpawned;
+	private bool gameOver;
+	private bool restart;
+	private float speedOffset;
+	private static int highScore = 0;
+
+	void Start()
+	{
+		score = 0;
+		shotsFired = 0;
+		gameOver = false;
+		restart = false;
+		restartText.text = string.Empty;
+		GameOverText.text = string.Empty;
+		GameStats.text = string.Empty;
+		speedOffset = 0f;
+		UpdateScore();
+		StartCoroutine(SpawnWaves());
+	}
+
+	void Update()
+	{
+		if(restart)
+		{
+			if(Input.GetKeyDown (KeyCode.R))
+				Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+		while(true)
+		{
+			for (int i = 0;i < hazardCount; i++)
+			{
+				++totalHazardsSpawned;
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				GameObject clone = (GameObject)Instantiate(hazard, spawnPosition, spawnRotation);
+				clone.rigidbody.AddForce(clone.transform.forward  * (InitialHazardSpeed - speedOffset));
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' to Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		if(score > highScore)
+			highScore = score;
+		UpdateScore();
+		if(score > 1 && score % 120 == 0)
+		{
+			hazardCount += Random.Range(1, 3);
+			speedOffset += 50f; //(Random.Range(-5f, 20f) * 10);
+		}
+	}
+
+	public void AddShot()
+	{
+		shotsFired++;
+	}
+
+	public void GameOver()
+	{
+		GameOverText.text = "Game Over!";
+		GameStats.text = GameStatsText();
+		gameOver = true;
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score.ToString();
+		HighScoreText.text = "High Score: " + highScore.ToString();
+	}
+
+	public string GameStatsText()
+	{
+		int tempScore = this.score/10;
+		float e1 = ((float)tempScore/(float)totalHazardsSpawned) * 100f;
+		float a1 = (((float)tempScore/(float)this.shotsFired)) * 100f;
+
+		return string.Format("Total Asteroids: {0}\nAsteroids Destroyed: {1} ({2}%)\nShots Fired: {3} ({4}%)",
+		                     totalHazardsSpawned.ToString(),
+		                     tempScore.ToString(),
+		                     e1.ToString("F1"),
+		                     this.shotsFired.ToString(),
+		                     a1.ToString("F1")
+		                     );
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax; // Clamp's boundaries
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed; // ship movement speed, available in unity
+	public float tilt;
+	public Boundary boundary; //reference of the boundary class.
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate = 0.5f;
+
+	private float nextFire = 0.0f;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if(gameControllerObject != null)
+			gameController = gameControllerObject.GetComponent<GameController>();
+		if(gameController == null)
+			Debug.Log ("Cannot find 'GameController' script");
+	}
+	
+	void Update()
+	{
+		if(Input.GetKey("space") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			gameController.AddShot();
+			//GameObject clone = 
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); //as GameObject;
+			audio.Play ();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal"); //gets user input
+		float moveVertical = Input.GetAxis ("Vertical"); // gets user input
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); // using vector to move horizonntal and vertical
+    //x,y,z axis - x for horiz, y is set to 0, z for vertical
+		rigidbody.velocity = movement * speed;//The object can now move with given speed
+    //Control of an object's position through physics simulation.
+		rigidbody.position = new Vector3
+		(
+      //mathf - collection of math functions
+      //Clamp - Clamps a value between a minimum float and maximum float value.
+      //gets 3 args - current, min, max
+			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f, 
+			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt); //tilts object arround axis 
+    //x velocity(speed)* -(tilt factor), otherwise ship will tilt in oposite direction
+	}
+
+
+}
