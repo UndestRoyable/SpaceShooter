875bb9b47f5215ae187078e9ce16fb6fa905d45a
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-23 19:46:27 +0200

    Explosion added. Asteroid can now fall down.
    Added two explosions - one when bolt hits the asteroid, and other when
    the spaceship hits the asteroid.
    Also, reusing Move script to move the asteroid.
    //Don't count 'asteroid' in this sentences =D

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+	public GameObject explosion;
+	public GameObject playerExplosion;
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.tag == "Boundary") //Ive tagged screen boundary and this is comparing other.tag to boundary tag 
 		{
 			return;//ends this function and returns control to Unity's game loop
 		}
+		Instantiate(explosion, transform.position, transform.rotation); // triggers the explosion
+		if (other.tag == "Player") //if player hits asteroid, we get a different explosion.
+		 {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		 }
 		Destroy(other.gameObject); //Destroys light bolt when hit the asteroid
 		Destroy (gameObject); //Destroys the asteroid, because this script is attached to Asteroid gameObject
 	}
