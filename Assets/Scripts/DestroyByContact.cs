3e3f1590ca43c458241bb8362026daf61f3b0f5f
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-23 19:13:35 +0200

    Asteroid spinning. Bolt can destroy asteroids
    Implemented asteroid's tumbling logic by using
    rigidbody.angularVelocity and a random value.
    Also the bolt can now destroy asteroids and then it's destroying itself.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary") //Ive tagged screen boundary and this is comparing other.tag to boundary tag 
+		{
+			return;//ends this function and returns control to Unity's game loop
+		}
+		Destroy(other.gameObject); //Destroys light bolt when hit the asteroid
+		Destroy (gameObject); //Destroys the asteroid, because this script is attached to Asteroid gameObject
+	}
+
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+	public float tumble; //holds max tumble value
+
+	void Start()
+	{
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;  
+			/*angularVelocity sets how fast rigidbody object is rotating
+			Doing it with Random.insideUnitSphere	- returns vector3 value that can be applied 
+			to rigidbody's angularVelocity*
+			Gets tumble value from the editor!*/
+	}
+}
