d32a0b614b0721ee874eb5a5124b9d8fef8d34d9
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-24 14:43:58 +0200

    Spawning waves of asteroids!!
    Tha GameController can now spawn multiple waves of multiple asteroids.
    There are some parameters like spawnWait, startWait, waveWait which can
    be set in the editor.
    Also explosion destroying by time added.

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+	public float lifetime;
+	void Start()
+	{
+		Destroy (gameObject, lifetime); // Destroys object after amount of time, specified as its lifetime
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,17 +6,32 @@ public class GameController : MonoBehaviour
 	public GameObject hazard;
 	public Vector3 spawnValues; // used to set random spawn position on X-axis.
 
+	public int hazardCount;
+	public float spawnWait;//wait time th throw next asteroid
+	public float startWait;//wait on game start so the player can get ready.
+	public float waveWait; //time to wait between the waves
+
 	void Start()
 	{
-		SpawnWaves (); //Calls SpawnWaves function on the start of the game
+		StartCoroutine(SpawnWaves ()); //Calls SpawnWaves function on the start of the game
 	}
-	void SpawnWaves()
+	IEnumerator SpawnWaves() //using coroutine
 	{
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
-		//Generates random hazard. y and z are static values, x is Randomly generated
-		//by Ramdon.Range function
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		yield return new WaitForSeconds(startWait);/*short pause in the beginning of the game, 
+		so the player will have time to get ready.*/
+		while(true)//a little bit of infinite loop :3 :D
+		{
+			for (int i = 0; i < hazardCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				//Generates random hazard. y and z are static values, x is Randomly generated
+				//by Ramdon.Range function
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait); // Waits some time before throw new asteroid.
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 
 }
